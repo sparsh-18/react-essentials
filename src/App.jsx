6f3f6301838bedefc57534c58133ae5391f3abc9
Sparsh Verma <sparsh.verma@viamagus.com>
2024-11-16 19:32:37 +0530

    feat: add Sidebar component and integrate with App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import Sidebar from "./components/Sidebar";
+
 function App() {
+
+  const projects = [
+    "Project 1", "Project 2", "Project 3"
+  ]
+
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8">
+      <Sidebar projects={projects}/>
+    </main>
   );
 }
 
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+export default function Sidebar({ projects }) {
+  const [selectedProject, setSelectedProject] = useState(null);
+
+  const handleProjectClick = (projectId) => {
+    console.log("Selected Project: ", projectId);
+    setSelectedProject(projectId);
+  };
+
+  return (
+    <aside className="bg-stone-900 text-stone-50 px-8 py-16 rounded-r-xl w-1/3 md:w-72">
+      <h2 className="uppercase mb-8 font-bold text-stone-200 md:text-xl tracking-wide">
+        Your Projects
+      </h2>
+      <div>
+        <button className="px-4 py-2 text-xs md:text-base bg-stone-700 hover:bg-stone-600 text-stone-300 hover:text-stone-100 rounded-md">
+          + Add Project
+        </button>
+      </div>
+      <ul className="my-8 text-stone-200">
+        {projects.map((project, index) => (
+          <li
+            key={index}
+            className={`my-2 px-2 py-1 hover:bg-stone-800 hover:cursor-pointer ${
+              selectedProject !== null && selectedProject === index ? "bg-stone-800" : ""
+            }`}
+            onClick={() => handleProjectClick(index)}
+          >
+            {project}
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
+}
