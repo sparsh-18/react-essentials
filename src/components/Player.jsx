6c771bf9bf1688143a9bfc92a7125642d3925a3b
Sparsh Verma <sparsh.verma@viamagus.com>
2024-11-16 13:51:08 +0530

    feat(player-component): add player name input and display functionality using ref and state

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,23 @@
+import {useRef, useState} from 'react';
+
 export default function Player() {
+  const playerNameRef = useRef('');
+  const [playerName, setPlayerName] = useState(null);
+
+  const handleSetName = () => {
+    setPlayerName(playerNameRef.current.value);
+    playerNameRef.current.value = '';
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {playerName ?? 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input
+          ref={playerNameRef} 
+          type="text" 
+        />
+        <button onClick={handleSetName}>Set Name</button>
       </p>
     </section>
   );
